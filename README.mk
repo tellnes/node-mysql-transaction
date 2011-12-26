17a2b031192247e1209b4a9ef4be4eca30cc69b9
Christian Tellnes <christian@tellnes.no>
2011-12-26 02:36:19 +0100

    first commit

---
+++ README.mk
@@ -0,0 +1,9 @@
+**Node MySQL Transaction**
+
+node-mysql-transaction implements transaction support for [node.js](https://github.com/Kijewski/node-mysql-pool) on top of René Kijewski's MySQL connection pool [node-mysql-pool](https://github.com/Kijewski/node-mysql-pool) which is implemented on top of Felix Geisendörfer's MySQL client [node-mysql](https://github.com/felixge/node-mysql).
+
+
+**Licence**
+
+node-mysql-transaction is licensed under the
+[MIT license](https://github.com/tellnes/node-mysql-transaction/blob/master/LICENSE).
---
+++ index.js
@@ -0,0 +1,61 @@
+var MySQLPool = require("mysql-pool").MySQLPool;
+
+var util = require("util");
+
+
+function Transaction(pool, client) {
+  this.pool = pool;
+  this.client = client;
+  client._transaction = true;
+}
+
+function makeFinishCb(that, cb) {
+  return function (err) {
+    that.client._transaction = false;
+    that.pool._poolAvail(that.client);
+    cb && cb(err);
+  };
+}
+
+Transaction.prototype.commit = function(cb) {
+  this.client.query('COMMIT', makeFinishCb(this, cb));
+};
+
+Transaction.prototype.rollback = function(cb) {
+  this.client.query('ROLLBACK', makeFinishCb(this, cb));
+};
+
+MySQLTransaction = function(properties) {
+  if(!(this instanceof MySQLTransaction)) {
+		return new MySQLTransaction(properties);
+	}
+	
+	MySQLPool.call(this, properties);
+	this._poolAvail = MySQLPool.prototype._avail.bind(this);
+}
+util.inherits(MySQLTransaction, MySQLPool);
+exports.MySQLTransaction = MySQLTransaction;
+
+MySQLTransaction.prototype.createClient = function createClient(options) {
+	return new MySQLTransaction(options);
+};
+
+
+MySQLTransaction.prototype._avail = function _avail(client) {
+  if (!client._transaction) {
+    this._poolAvail(client);
+  }
+};
+
+MySQLTransaction.prototype.transaction = function(cb) {
+  var client = this._idleQueue.shift();
+	if(!client) {
+		pool._todoQueue.push({method:wrapperMethod, args:args});
+		return pool;
+	}
+
+  var trans = new Transaction(this, client);
+  client.query('START TRANSACTION', function(err) {
+	  cb(err, trans);
+	});
+};
